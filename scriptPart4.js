dc27c9ba579576834c749802bf46503e168f9316
Larysa Rud <larpavrud@gmail.com>
2024-04-18 21:43:06 -0400

    Part 4: Sorting and Manipulating Data

---
+++ scriptPart4.js
@@ -0,0 +1,2 @@
+//Part 4: Sorting and Manipulating Data
+let inputArray = [{ id: "42", name: "Bruce", occupation: "Knight", age: "41" },  { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },  { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },  { id: "98", name: "Bill", occupation: "Doctor’s Assistant", age: "26" }];
